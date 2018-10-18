ade384c47e1a2d60af2b74ccaa811581efb677a6
e-barr <31676765+e-barr@users.noreply.github.com>
2018-10-17 23:35:08 -0500

    update

--- not-buggy-skipe_front-end/src/containers/MeetingRoom.js
+++ not-buggy-skipe_front-end/src/containers/MeetingRoom.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react'
 
 class MeetingRoom extends Component {
     render() {
+        const styles = {
+            width: '',
+            height: '640px'
+        }
+
         console.log(this.props)
         if (this.props.room) {
             return (
                 <div>
                     WELCOME TO THE ROOM CALLED {this.props.room.name}
+                   
+
+                    <iframe title={this.props.room.name}
+                        src="https://tokbox.com/embed/embed/ot-embed.js?embedId=cb6af7f6-5ac4-4691-9850-07636964bfb7&room=DEFAULT_ROOM&iframe=true"
+                        width="66%"
+                        height="66%"
+                        allow="microphone; camera"
+                    ></iframe>
+    
                 </div>
             )
         } else {
