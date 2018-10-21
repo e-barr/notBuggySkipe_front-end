aa909fdfb3cf408fd84c34631d5e77984f15508f
e-barr <31676765+e-barr@users.noreply.github.com>
2018-10-21 17:16:43 -0500

    update contact us page

--- not-buggy-skipe_front-end/src/containers/ContactUs.js
+++ not-buggy-skipe_front-end/src/containers/ContactUs.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ContactUs = props => {
     return (
         <div>
-            THIS IS THE CONTACT US PAGE.
+            If you have any questions, please feel free to contact me at <a href="https://github.com/e-barr">e-barr</a>.
         </div>
     )
 }
