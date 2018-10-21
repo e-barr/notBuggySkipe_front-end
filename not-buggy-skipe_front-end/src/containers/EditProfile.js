385fa049e37df996b59f26f5743837311b942d0f
e-barr <31676765+e-barr@users.noreply.github.com>
2018-10-21 17:24:16 -0500

    update editprofile

--- not-buggy-skipe_front-end/src/containers/EditProfile.js
+++ not-buggy-skipe_front-end/src/containers/EditProfile.js
@@ -17,16 +17,17 @@ class EditProfile extends Component {
     }
 
     render() {
-        console.log(this)
+        const { closeEditProfile, profileChangesConfirmed } = this.props
+        const { image_url, username, city, country, email } = this.state
         return (
             <div className="edit-profile-tile">
-            <button onClick={this.props.closeEditProfile}>X</button>
-            <form onSubmit={() => this.props.profileChangesConfirmed(this.state)}>
-                <p>Image URL:<input type="text" name="image_url" value={this.state.image_url} onChange={this.onChange} /></p>
-                <p>Username:<input type="text" name="username" value={this.state.username} onChange={this.onChange} /></p>
-                <p>City:<input type="text" name="city" value={this.state.city} onChange={this.onChange} /></p>
-                <p>Country:<input type="text" name="country" value={this.state.country} onChange={this.onChange} /></p>
-                <p>Email:<input type="text" name="email" value={this.state.email} onChange={this.onChange} /></p>
+            <button onClick={closeEditProfile}>X</button>
+            <form onSubmit={() => profileChangesConfirmed(this.state)}>
+                <p>Image URL:<input type="text" name="image_url" value={image_url} onChange={this.onChange} /></p>
+                <p>Username:<input type="text" name="username" value={username} onChange={this.onChange} /></p>
+                <p>City:<input type="text" name="city" value={city} onChange={this.onChange} /></p>
+                <p>Country:<input type="text" name="country" value={country} onChange={this.onChange} /></p>
+                <p>Email:<input type="text" name="email" value={email} onChange={this.onChange} /></p>
                 <button>Confirm Changes</button>
 
             </form>
