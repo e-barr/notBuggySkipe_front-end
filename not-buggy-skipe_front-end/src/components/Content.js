93ea89da31e811fb983132eaa44b83b3432953e8
e-barr <31676765+e-barr@users.noreply.github.com>
2019-02-07 21:54:55 -0600

    render profileTile

--- not-buggy-skipe_front-end/src/components/Content.js
+++ not-buggy-skipe_front-end/src/components/Content.js
@@ -10,15 +10,37 @@
 // export default Content
 
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import ProfileTile from './ProfileTile'
 
 class Content extends Component {
     render() {
-        return (
-            <div className="ui raised padded text container segment">
-                content
-            </div>
-        )
+        if (this.props.user) {
+            const { username, city, country, image_url } = this.props.user
+            console.log(this.props.user)
+            return (
+                <div className="ui raised padded text container segment">
+                    <div className="ui cards">
+                        <ProfileTile
+                            username={username}
+                            city={city}
+                            country={country}
+                            image_url={image_url}
+                        />
+                    </div>
+                </div>
+            )
+        } else {
+            return null
+        }
     }
 }
 
-export default Content;
+const mapStateToProps = (state) => {
+    // console.log(state)
+    return {
+        user: state.auth.currentUser
+    }
+}
+
+export default connect(mapStateToProps)(Content);
--- not-buggy-skipe_front-end/src/components/ProfileTile.js
+++ not-buggy-skipe_front-end/src/components/ProfileTile.js
@@ -1,19 +1,52 @@
+// import React, { Component } from 'react'
+
+// class ProfileTile extends Component {
+//     render () {
+//         const { username, city, country, email, image_url } = this.props.user
+//         return (
+//             <div>
+//                 <div>
+//                     <h2><img src={image_url} width="10%" height="10%" alt="user profile" />{username}</h2>
+//                     <ul>{city}, {country}</ul>
+//                     <ul>{email}</ul>
+//                     <button onClick={this.props.editUser}>Edit</button><button onClick={this.props.logout}>Logout</button>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
+
+// export default ProfileTile
+
 import React, { Component } from 'react'
 
 class ProfileTile extends Component {
-    render () {
-        const { username, city, country, email, image_url } = this.props.user
-        return (
-            <div>
-                <div>
-                    <h2><img src={image_url} width="10%" height="10%" alt="user profile" />{username}</h2>
-                    <ul>{city}, {country}</ul>
-                    <ul>{email}</ul>
-                    <button onClick={this.props.editUser}>Edit</button><button onClick={this.props.logout}>Logout</button>
+    render() {
+        console.log(this.props)
+        if (this.props.username) {
+            const { username, city, country, image_url } = this.props
+            return (
+                <div className="ui card">
+                    <div className="image">
+                        <img 
+                        src={image_url}
+                        alt={`${username}`}
+                        />
+                    </div>
+                    <div className="content">
+                        <div className="header">
+                            <h2>{username}</h2>
+                        </div>
+                        <div className="description">
+                            <p>{`${city}, ${country}`}</p>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        )
+            )
+        } else {
+            return null
+        }
     }
 }
 
-export default ProfileTile
+export default ProfileTile;
