212963aac98c3cbce1c23c28c2c76bb2a0386b46
e-barr <31676765+e-barr@users.noreply.github.com>
2019-02-14 20:38:46 -0600

    add button on ViewAddContacts component

--- not-buggy-skipe_front-end/src/components/ViewAddContacts.js
+++ not-buggy-skipe_front-end/src/components/ViewAddContacts.js
@@ -17,13 +17,16 @@ class ViewAddContacts extends Component {
         const contacts = this.props.allUsers
         return contacts.map(contact => {
             return (
-                <div className="item" key={contact.id} >
+                <div className="item" key={contact.id}>
+                    <div className="right floated content">
+                        <button className="ui right floated button">Add</button>
+                    </div>
                     <img
                         className="ui avatar image"
                         src={contact.image_url}
                     />
                     <div className="content">
-                        <div className="header">{contact.username}</div>
+                        {contact.username}
                     </div>
                 </div>
             )
@@ -35,9 +38,12 @@ class ViewAddContacts extends Component {
             <div className="ui raised padded container segment">
                 <h2>ViewAddContacts</h2>
                 <div
-                    className="ui middle aligned selection list"
+                    className="ui middle aligned divided list"
                 >
                     {this.renderPossibleContacts()}
+                    <br></br>
+                    <br></br>
+                    <br></br>
                 </div>
                 <button
                     className="ui right floated button"
